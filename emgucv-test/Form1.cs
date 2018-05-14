d266c6a3a6ce8baeaa9b750f4437102f5969aac7
Weichun <weichun@csie.tw>
2018-05-14 18:01:03 +0800

    init

---
+++ emgucv-test/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace emgucv_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long matchTime;
+            using (Mat modelImage = CvInvoke.Imread(@"opencv\data\sample\box.png", ImreadModes.Grayscale))
+            using (Mat observedImage = CvInvoke.Imread(@"opencv\data\sample\box_in_scene.png", ImreadModes.Grayscale))
+            {
+                Mat result = DrawMatches.Draw(modelImage, observedImage, out matchTime);
+                ImageViewer.Show(result, String.Format("Matched in {0} milliseconds", matchTime));
+            }
+        }
+    }
+}
---
+++ emgucv-test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+
+namespace emgucv_test
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            long matchTime;
+            using (Mat modelImage = CvInvoke.Imread(@"opencv\data\sample\box.png", ImreadModes.Grayscale))
+            using (Mat observedImage = CvInvoke.Imread(@"opencv\data\sample\box_in_scene.png", ImreadModes.Grayscale))
+            {
+                Mat result = DrawMatches.Draw(modelImage, observedImage, out matchTime);
+                ImageViewer.Show(result, String.Format("Matched in {0} milliseconds", matchTime));
+            }
+        }
+    }
+}
